a26fa624674a2226fdbf34d8c029398f20d641a0
Shihab Sagor <shihabsagor963@gamil.com>
2024-09-25 00:47:35 +0600

    function commit

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 document.getElementById('blog-page').addEventListener('click',function(){
     window.location.href = '/blog.html';
 })
+
 const donationTab = document.getElementById('donation-btn')
 donationTab.addEventListener('click',function(){
     // donationTab.classList.add('font-extrabold','bg-lime-300')
     donationTab.classList.add('font-bold','bg-lime-300')
     historyTab.classList.remove('font-bold','bg-lime-300')
+    donationField.classList.remove('hidden')
 })
+const donationField =document.getElementById('donation-field')
 const historyTab = document.getElementById('history-btn')
 historyTab.addEventListener('click',function(){
     historyTab.classList.add('font-bold','bg-lime-300')
     donationTab.classList.remove('font-bold','bg-lime-300')
+    donationField.classList.add('hidden')
+})
+const inputBtn = document.getElementById('input-btn')
+inputBtn.addEventListener('click',function(){
+const inputAmount = parseFloat(document.getElementById('input-amount').value);
+const placeAmount = parseFloat(document.getElementById('add-amount').innerText);
+const addingAmount = inputAmount + placeAmount;
+const addingField = document.getElementById('add-amount')
+addingField.innerText = addingAmount;
+const totalBalance = parseFloat(document.getElementById('total-balance').innerText);
+const lessBalance = totalBalance - inputAmount ;
+const totalBalanceOfAcc = document.getElementById('total-balance');
+totalBalanceOfAcc.innerText = lessBalance; 
+
+if( Number.isNaN(inputAmount) || inputAmount < 0 ){
+    console.log('please enter the valid amount');
+    alert('Invalid Donate amount')
+    document.getElementById('modal').classList.add('hidden')
+    addingField.innerText = addingAmount
+}
+
+// alert('please enter a valid amount')
+
 })
