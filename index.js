01a859d7db7ad814f423da83d64ba50428946651
Shihab Sagor <shihabsagor963@gamil.com>
2024-09-25 12:00:19 +0600

    history section

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ document.getElementById('blog-page').addEventListener('click',function(){
     window.location.href ='./blog.html';
     
 })
-
+const historyField = document.getElementById('history-container')
 const donationTab = document.getElementById('donation-btn')
 donationTab.addEventListener('click',function(){
     // donationTab.classList.add('font-extrabold','bg-lime-300')
     donationTab.classList.add('font-bold','bg-lime-300')
     historyTab.classList.remove('font-bold','bg-lime-300')
     donationField.classList.remove('hidden')
+    historyField.classList.add('hidden')
+    
 })
 const donationField =document.getElementById('donation-field')
 const historyTab = document.getElementById('history-btn')
@@ -16,6 +18,7 @@ historyTab.addEventListener('click',function(){
     historyTab.classList.add('font-bold','bg-lime-300')
     donationTab.classList.remove('font-bold','bg-lime-300')
     donationField.classList.add('hidden')
+    historyField.classList.remove('hidden')
 })
 const inputBtn = document.getElementById('input-btn')
 inputBtn.addEventListener('click',function(){
@@ -36,6 +39,17 @@ if( Number.isNaN(inputAmount) || inputAmount < 0 ){
     addingField.innerText = addingAmount;
     placeAmount == placeAmount;
 }
+
+const historyFunction = document.createElement("div")
+historyFunction.className ='bg-white p-3 rounded-md border mt-2 text-left border-gray-400'
+historyFunction.innerHTML = `
+<p class="text-lg font-bold  ">${inputAmount} Taka is Donated for famine-2024 at Noakhali ,bangladesh</p>
+<p class="text-sm text-gray-500 " >Date: ${new Date().toDateString()} GMT +0600 (Bangladesh standard time) </p>
+`;
+const historyContainer = document.getElementById('history-list')
+historyContainer.insertBefore(historyFunction, historyContainer.lastChild);
+
+
 });
  document.getElementById('feni-btn').addEventListener('click',function(){
 const inputAmount = parseFloat(document.getElementById('feni-input').value);
@@ -54,6 +68,14 @@ if( Number.isNaN(inputAmount) || inputAmount < 0 ){
     document.getElementById('modal').classList.add('-z-index-1')
     addingField.innerText = addingAmount
 }
+const historyFunction = document.createElement("div")
+historyFunction.className ='bg-white p-3 rounded-md border mt-2 text-left border-gray-400'
+historyFunction.innerHTML = `
+<p class="text-lg font-bold  ">${inputAmount} Taka is Donated for famine-2024 at flood relief in feni,bangladesh</p>
+<p class="text-sm text-gray-500 " >Date: ${new Date().toDateString()} GMT +0600 (Bangladesh standard time) </p>
+`;
+const historyContainer = document.getElementById('history-list')
+historyContainer.insertBefore(historyFunction, historyContainer.lastChild);
 
  })
 
@@ -74,6 +96,16 @@ if( Number.isNaN(inputAmount) || inputAmount < 0 ){
         document.getElementById('modal').classList.add('hidden')
         addingField.innerText = addingAmount
     }
+    const historyFunction = document.createElement("div")
+historyFunction.className ='bg-white p-3 rounded-md border mt-2 text-left border-gray-400'
+historyFunction.innerHTML = `
+<p class="text-lg font-bold  ">${inputAmount} Taka is Donated for Aid for Injured in the Quota Movement, bangladesh</p>
+<p class="text-sm text-gray-500 " >Date: ${new Date().toDateString()} GMT +0600 (Bangladesh standard time) </p>
+`;
+const historyContainer = document.getElementById('history-list')
+historyContainer.insertBefore(historyFunction, historyContainer.lastChild);
+
+
     
      })
     
